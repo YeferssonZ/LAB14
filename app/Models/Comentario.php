0b0c03969f7540315aa4625c8dd2cbe75b3ad6f2
Yefersson <yefersson.zuniga@tecsup.edu.pe>
2022-11-23 16:48:19 -0500

    LAB 14

---
+++ app/Models/Comentario.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace App\Models;
+ 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+ 
+class Comentario extends Eloquent
+{
+    use HasFactory;
+    public function Foto(){
+        return $this->belongsTo(Foto::class);
+    }
+    public function User(){
+        return $this->belongsTo(User::class);
+    }
+}
